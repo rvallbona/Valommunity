da122b8991973a052c4703700a95cf11a8f65c4f
Ramon Vallbona <98883990+rvallbona@users.noreply.github.com>
2023-02-28 13:23:38 +0100

    Recover password finalizado.

--- app/src/main/java/com/example/valommunityapp/RecordarPassActivity.kt
+++ app/src/main/java/com/example/valommunityapp/RecordarPassActivity.kt
@@ -1,7 +1,11 @@
 package com.example.valommunityapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -13,7 +17,23 @@ class RecordarPassActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recordar_pass)
-
+        val recoverEmail : TextView = findViewById(R.id.recoverEmail)
+        val recoverPassButton : Button = findViewById(R.id.sendRecoverPass)
+        recoverPassButton.setOnClickListener {
+            sendPasswordReset(recoverEmail.text.toString())
+        }
         firebaseAuth = Firebase.auth
+    }
+    private fun sendPasswordReset(email: String){
+        firebaseAuth.sendPasswordResetEmail(email)
+            .addOnCompleteListener(){ task ->
+            if (task.isSuccessful){
+                Toast.makeText(baseContext, "Correo de cambio de contraseña", Toast.LENGTH_SHORT).show()
+                val i = Intent(this, AuthActivity::class.java)
+                startActivity(i)
+            }else{
+                Toast.makeText(baseContext, "ERROR: No se pudo completar el proceso", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
